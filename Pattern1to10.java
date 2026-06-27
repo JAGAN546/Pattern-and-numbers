e5efa05499b2d2e9e8ef48ce6db6985506aa36df
JAGAN546 <kjagannathsubudhi4@gmail.com>
2026-06-27 10:22:46 +0530

    Added pattern 1 to 0

---
+++ Pattern1to10.java
@@ -0,0 +1,14 @@
+public class Pattern1to10 {
+    public static void main(String[] args) {
+        int i,j,k=1;
+        for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println(" ");
+        }
+    }
+}
